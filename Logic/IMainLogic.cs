bdda601e720014fe756c36f82d106911dae8fa55
vmarcell96 <vajdam96@gmail.com>
2023-01-28 11:45:14 +0100

    FEAT: created MainLogic class which implements IMainLogic interface and uses UIPrinter and VentFileParser classes

---
+++ Logic/IMainLogic.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydrothermalJunctionDetector.Logic
+{
+    internal interface IMainLogic
+    {
+        string GetInputFileLocation();
+        void ValidateFile();
+
+        void GetCoordinates();
+        void CalculateCrossingPoints();
+        void ReportCrossingPoints();
+        void WriteOutReport();
+    }
+}
