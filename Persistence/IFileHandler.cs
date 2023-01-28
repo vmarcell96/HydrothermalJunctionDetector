c573e23f3a015ac99ec2f5d73eec5bd03a6d1eed
vmarcell96 <vajdam96@gmail.com>
2023-01-28 11:43:11 +0100

    FEAT: created FileHandler class which implements IFileHandler interface

---
+++ Persistence/IFileHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydrothermalJunctionDetector.Persistence
+{
+    internal interface IFileHandler
+    {
+        string ReadFile(string fileLocation);
+        void WriteFile(string outputLocation);
+    }
+}
