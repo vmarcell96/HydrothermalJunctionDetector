835be9e880a5ee56acc66febf056d69696af595b
vmarcell96 <vajdam96@gmail.com>
2023-01-28 11:42:47 +0100

    FEAT: created UIPrinter class which implements UIPrinter interface and uses consoleLogger class

---
+++ UI/UIPrinter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydrothermalJunctionDetector.UI
+{
+    internal class UIPrinter : IUIPrinter
+    {
+        private readonly ILogger _consoleLogger;
+        
+        public UIPrinter(ILogger consoleLogger)
+        {
+            _consoleLogger= consoleLogger;
+        }
+        public void PrintError(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintMainMenu()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintMessage(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void PrintProgressBar(int percentLoaded)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
