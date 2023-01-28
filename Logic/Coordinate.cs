f193d562ecce98211ca170a2523aca19b4a54094
vmarcell96 <vajdam96@gmail.com>
2023-01-28 11:41:01 +0100

    FEAT: created coordinate struct

---
+++ Logic/Coordinate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HydrothermalJunctionDetector.Logic
+{
+    internal struct Coordinate
+    {
+        //float takes up less space than double, and in the example we only use one number after the decimal point
+        public float X;
+        public float Y;
+    }
+}
