6e0c0738ffee45ab2b4f9d4a67ace4e62826bd9f
vmarcell96 <vajdam96@gmail.com>
2023-01-30 22:53:09 +0100

    REFACTOR: using float tuple instead of Point struct

--- Logic/VentLine.cs
+++ Logic/VentLine.cs
@@ -9,7 +9,12 @@ namespace HydrothermalJunctionDetector.Logic
     internal struct VentLine
     {
         //float takes up less space than double, and in the example we only use one number after the decimal point
-        public Coordinate Start;
-        public Coordinate End;
+        public (float, float) StartPoint;
+        public (float, float) EndPoint;
+
+        public override string ToString()
+        {
+            return $"StartPoint: ({StartPoint.Item1},{StartPoint.Item2}) EndPoint: ({EndPoint.Item1},{EndPoint.Item2}).";
+        }
     }
 }
