84554c35d61bb703b154e5b99e189f0f56ecaca6
vmarcell96 <vajdam96@gmail.com>
2023-01-30 22:55:10 +0100

    FEAT: basic exception handling in program.cs, testing functionality

--- Program.cs
+++ Program.cs
@@ -4,16 +4,38 @@ using HydrothermalJunctionDetector.Logic;
 using HydrothermalJunctionDetector.Persistence;
 using HydrothermalJunctionDetector.UI;
 
-Console.WriteLine("Hello, World!");
 
 ILogger consoleLogger = new ConsoleLogger();
 IFileHandler fileHandler = new FileHandler();
-IFileParser fileParser = new VentFileParser(fileHandler);
+IFileParser fileParser = new HydrothermicVentFileParser(fileHandler);
 IUIPrinter uiPrinter = new UIPrinter(consoleLogger);
 
 IMainLogic mainLogic = new MainLogic(fileParser, uiPrinter);
 
 
 
-fileHandler.ReadFile();
+
+
+try
+{
+
+    //Test not valid txt file
+    var ventLines = fileParser.ParseFile(@"..\..\..\InputFileLineSegments_NotValid.txt");
+
+    //var ventLines = fileParser.ParseFile();
+    //foreach (var item in ventLines)
+    //{
+    //    Console.WriteLine(item.ToString());
+    //}
+    var crossingPoints = mainLogic.CalculateCrossingPoints(ventLines);
+    mainLogic.ReportCrossingPoints(crossingPoints);
+
+
+    
+}
+catch (Exception e)
+{
+
+    Console.WriteLine(e.Message);
+}
 
