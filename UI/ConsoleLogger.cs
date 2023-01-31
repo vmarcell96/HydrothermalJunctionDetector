d3f583d422fd472d9a7a11f3173cf1f4f6a15103
vmarcell96 <vajdam96@gmail.com>
2023-01-31 23:31:26 +0100

    FEAT: implemented LogWrite,LogWriteLine functions

--- UI/ConsoleLogger.cs
+++ UI/ConsoleLogger.cs
@@ -8,9 +8,19 @@ namespace HydrothermalJunctionDetector.UI
 {
     internal class ConsoleLogger : ILogger
     {
-        public void Log(string message)
+        public void LogWriteLine(string message)
         {
             Console.WriteLine(message);
+        }
+
+        public void LogWrite(string message)
+        {
+            Console.Write(message);
+        }
+
+        public void LogError(string message)
+        {
+            Console.Write(message);
         }
     }
 }
